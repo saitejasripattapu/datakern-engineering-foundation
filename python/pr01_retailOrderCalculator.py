bb4dfb8bc5d71481569795b74bb57844d059da8e
Teja <p.saitejasri@gmail.com>
2026-09-11 22:06:03 +0200

    First Program in Python

---
+++ python/pr01_retailOrderCalculator.py
@@ -0,0 +1,12 @@
+customer_name = "Teja"
+product_name = "DE books"
+quantity = 3
+unit_price = 4.50
+
+total_amount = quantity * unit_price
+
+print("Customer:", customer_name)
+print("Product:", product_name)
+print("Quantity:", quantity)
+print(f"Unit price: €{unit_price:.2f}")
+print(f"Total amount: €{total_amount:.2f}")
